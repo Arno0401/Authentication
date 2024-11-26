b93e709a18a21bf38127bf63efa50db58285b7fb
Arno0401 <school898b@gmail.com>
2024-11-26 16:57:55 +0500

    add .gitignore and fix DB

--- db/db.go
+++ db/db.go
@@ -2,14 +2,39 @@ package db
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 )
+
+type Config struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+	DBName   string `json:"dbname"`
+	SSLMode  string `json:"sslmode"`
+}
 
 var DB *sql.DB
 
 func ConnectDB() {
-
-	db, err := sql.Open("postgres", "user=arno password=909209866sh dbname=arno_db sslmode=disable")
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalf("Ошибка открытия файла конфигурации: %v", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Println("Ошибка при закрытии файла конфигурации:", err)
+		}
+	}(file)
+	var config Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		log.Fatalf("Ошибка декодирования JSON: %v", err)
+	}
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", config.User, config.Password, config.DBName, config.SSLMode)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
 	}
