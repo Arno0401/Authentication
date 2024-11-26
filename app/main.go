9a0d3457bcacc4eabed63cc4566d027d7be963ee
Arno0401 <school898b@gmail.com>
2024-11-26 16:39:54 +0500

    cmd->app

---
+++ app/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"converter.go/db"
+	"converter.go/handler"
+	"converter.go/middleware"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db.ConnectDB()
+
+	http.HandleFunc("/sign-up", handler.SignUpHandler)
+	http.HandleFunc("/change", handler.ChangePassword)
+	http.HandleFunc("/sign-in", handler.SignInHandler)
+	http.HandleFunc("/get-info", handler.GettokenInfo)
+	http.HandleFunc("/refresh", handler.RefreshToken)
+	http.HandleFunc("/my-info", handler.MyInfo)
+	http.Handle("/users", middleware.AdminMiddleware(http.HandlerFunc(handler.GetUsers)))
+
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Println("Error listening:", err)
+		return
+	}
+
+	defer func(DB *sql.DB) {
+		err := DB.Close()
+		if err != nil {
+			log.Println("Ошибка при закрытии базы данных:", err)
+		}
+	}(db.DB)
+
+}
